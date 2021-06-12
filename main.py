f97c3a045e4b581686462cbddb2315e1b6dc8e2e
jelisca <jelisacao@gmail.com>
2021-06-12 18:51:34 -0400

    Added scoring. Removed unused code and comments.

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ import sys
 import time
 import random
 
+# initializes display mode
+pygame.init()
+
 LENGTH = 2000
 HEIGHT = 1000
 WHEEL_RADIUS = 210
@@ -19,11 +22,8 @@ FPS = 40
 FramePerSecond = pygame.time.Clock()
 
 SCREEN = pygame.display.set_mode([LENGTH, HEIGHT])
-pygame.display.set_caption("Singleplayer Taiko DDR")
-
-MULTIPLAYER = False
-DIFF_SEL = 0
-SCORE = 0
+pygame.display.set_caption("Play")
+my_font = pygame.font.SysFont("arial", 40)
 
 #TODO:colour selection for each player
 #TODO:Aproject spawning for both players
@@ -38,6 +38,7 @@ class ArrowNote(pygame.sprite.Sprite):
         self.color    = note_attrs['color']
         self.delay    = note_attrs['delay']
         self.gst      = game_start_time
+        self.score    = 0
 
         super().__init__()
 
@@ -50,6 +51,13 @@ class ArrowNote(pygame.sprite.Sprite):
             self.pos = self.rect.center = LEFT
         else:
             self.pos = self.rect.center = RIGHT
+
+    def add_score(self):
+        if self.pos[0] > LENGTH/2 - WHEEL_RADIUS and self.pos[0] < LENGTH/2 + WHEEL_RADIUS:
+            if self.pos[0] > LENGTH/2 - WHEEL_RADIUS/3 and self.pos[0] < LENGTH/2 + WHEEL_RADIUS/3:
+                self.score += 100
+            else:
+                self.score += 50
 
     def can_deploy(self):
         if time.time() - self.gst >= self.delay:
@@ -77,19 +85,20 @@ class ArrowNote(pygame.sprite.Sprite):
     def key_is_pressed(self):
         key_state = pygame.key.get_pressed()
         if self.velocity > 0:
-            if key_state[pygame.K_a]:
+            if key_state[pygame.K_a] or key_state[pygame.K_LEFT]:
                 return True
             else:
                 return False
         else:
-            if key_state[pygame.K_s]:
+            if key_state[pygame.K_d] or key_state[pygame.K_RIGHT]:
                 return True
             else:
                 return False
 
     def kill(self):
         # check ball velocity to determine key to use for killing
-        # after killing, reset self.gst
+        # after killing, reset self.gst'
+        self.add_score()
         if self.velocity > 0:
             self.rect.center = LEFT
             self.delay = random.randint(0, 3)
@@ -107,89 +116,10 @@ class ArrowNote(pygame.sprite.Sprite):
             self.kill()
 
 
-class Blah:
-
-    miss = False
-    lock = True
-    killed = False
-
-    def ___init__(self, vel, rad, colour, start, delay_time):
-        self.vel = vel
-        self.rad = rad
-        self.colour = colour
-        self.start = start
-        self.delay_time = delay_time
-
-        #https://www.101computing.net/creating-sprites-using-pygame/
-        #referenced for sprite creation
-        super().__init__()
-
-        self.image = pygame.Surface([self.rad*2, self.rad*2])
-        self.rect = self.image.get_rect()
-        self.image.fill('white')
-        self.image.set_colorkey('white')
-        pygame.draw.circle(self.image, self.colour, self.rect.center, self.rad)
-        self.pos = self.rect.center = self.start
-
-    def move(self):
-        self.rect.move_ip([self.vel, 0])
-        self.pos = self.rect.center
-        SCREEN.blit(self.image, self.rect)
-
-    def key_check(self):
-        if self.miss == False:
-            if self.lock == False:
-                events = pygame.event.get()
-                for event in events:
-                    if event.type == pygame.KEYDOWN:
-                        print(self.colour)
-                        print(self.killed)
-                        if event.key == pygame.K_a or event.key == pygame.K_s:
-                            self.miss = True
-                            return False
-                        else:
-                            return True
-                    else:
-                        return True
-            else:
-                return True
-
-    def border_check(self):
-        if self.start == RIGHT:
-            if self.pos[0] <= LENGTH/2 + WHEEL_RADIUS * self.vel/abs(self.vel):
-                return False
-            else:
-                return True
-        elif self.start == LEFT:
-            if self.pos[0] >= LENGTH/2 + WHEEL_RADIUS * self.vel/abs(self.vel):
-                return False
-            else:
-                return True
-
-    def can_kill(self, starttime):
-        if self.deploy_check(starttime) and self.lock is False:
-            return True
-        else:
-            return False
-
-    def update(self, starttime):
-        if self.deploy_check(starttime) and not self.killed:
-            if self.border_check() and not self.key_check():
-                self.move()
-            else:
-                if self.can_kill(starttime):
-                    print(f'{self.colour} is killed')
-                    self.killed = True
-                    self.kill()
-
-
 class PlayerWheel(pygame.sprite.Sprite):
 
     def __init__(self):
-        if MULTIPLAYER:
-            pass
-        else:
-            self.pos = [LENGTH/2, HEIGHT/2]
+        self.pos = [LENGTH/2, HEIGHT/2]
 
         super().__init__()
         self.image = pygame.Surface([WHEEL_RADIUS * 2, WHEEL_RADIUS * 2])
@@ -205,51 +135,21 @@ class PlayerWheel(pygame.sprite.Sprite):
 
 default_note_attrs = {'velocity': 10, 'radius': WHEEL_RADIUS/3, 'color': 'red', 'delay': 0}
 
-
-#  class NotesQueue:
-
-#      def __init__(self):
-#          self.notes_queue = []
-#          #  self.start_time  = time.time()
-
-#      def put_note(self, note):
-#          #  note = ArrowNote(default_note_attrs, self.start_time)
-#          if note.can_deploy():
-#              self.notes_queue.append(note)
-
-#      def remove_note(self):
-#          self.notes_queue.pop(0)
-
-#      def move_notes(self):
-#          print(len(self.notes_queue))
-#          for note in self.notes_queue:
-#              note.move()
-
-
 def main():
     #starting time of game
     start_time = time.time()
 
-    #  nq = NotesQueue()
-    #  note_gen = generate_notes(10000, start_time)
-
-    #  note = ArrowNote(default_note_attrs, start_time)
-    #  nq.put_note(note)
-    #  default_note_attrs.update({'velocity': 1, 'delay': 2, 'color': 'green'})
-    #  note = ArrowNote(default_note_attrs, start_time)
-
     left_note  = ArrowNote({'velocity': 10, 'radius': WHEEL_RADIUS/3, 'color': 'red', 'delay': random.randint(0, 3)}, start_time)
     right_note = ArrowNote({'velocity': -10, 'radius': WHEEL_RADIUS/3, 'color': 'green', 'delay': random.randint(0, 3)}, start_time)
-
-    # initializes display mode
-    pygame.init()
 
     target = PlayerWheel()
 
     # game loop
     while True:
-        #  note = yield next(note_gen)
 
+        score_display = my_font.render('Score: ' + str(left_note.score + right_note.score), True, 'white', 'black')
+        score_rect = score_display.get_rect()
+        score_rect.center = (LENGTH / 2, HEIGHT / 8)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -258,15 +158,12 @@ def main():
 
         SCREEN.fill('white')
         target.update()
+        SCREEN.blit(score_display, score_rect)
 
         if left_note.can_deploy():
-            # left_note.move()
             left_note.update()
         if right_note.can_deploy():
-            # right_note.move()
             right_note.update()
-        #  nq.move_notes()
-
 
         pygame.display.update()
 
